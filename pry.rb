8c9f2af57508fa835ed7ff7e128fb239784f4c42
Rachel Bock <rachel.online@gmail.com>
2022-10-06 09:01:07 -0600

    Update the pry.rb file to add the loop around binding.pry

--- pry.rb
+++ pry.rb
@@ -4,4 +4,7 @@ favorite_things = ["Trapper keeper", "Netscape Navigator", "Troll Doll"]
 binding.pry
 new_thing = "Banana Split"
 favorite_things << new_thing
-binding.pry
+25.times do 
+  p favorite_things
+  binding.pry
+end
