e1dc0ab70c97b7789f424d73a462dfb2eec329ea
Rachel Bock <rachel.online@gmail.com>
2022-10-06 09:23:10 -0600

    Add comment on how to exit loop

--- pry.rb
+++ pry.rb
@@ -5,6 +5,6 @@ binding.pry
 new_thing = "Banana Split"
 favorite_things << new_thing
 25.times do 
-  p favorite_things
+  p favorite_things   #Use exit! to exit the loop without having to go through all 25 binding.pry statements.
   binding.pry
 end
